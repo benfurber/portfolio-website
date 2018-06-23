89c0bd21bc4ea588accd7a940a64735dd2ce400f
benfurber <ben@furber-consultancy.com>
2018-06-23 16:37:14 +0100

    Add Enzyme for better testing

--- src/AppHeader/AppHeader.test.js
+++ src/AppHeader/AppHeader.test.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow, mount, render } from 'enzyme';
+
 import AppHeader from './AppHeader';
 
 it('renders without crashing', () => {
