7abc0c53f3c471b6a52ded6b15f5f98f3173e1e5
benfurber <ben@furber-consultancy.com>
2018-06-24 18:36:47 +0100

    Add Contact Content

--- src/ContentBody/ContactContent.js
+++ src/ContentBody/ContactContent.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react'
+import { Container, Divider, Icon } from 'semantic-ui-react'
 
 class ContactContent extends Component {
   render() {
     return (
-      <Container className='ContactContent'>
-        ContactContent
+      <Container text textAlign='center' className='ContactContent'>
+        <h2>Contact Ben</h2>
+        <Divider hidden />
+        <a href='mailto:ben@fu.rber.io'>
+          <Icon name='at' size='big' /> ben@fu.rber.io
+        </a>
+        <Divider hidden />
+        <a href='tel:07704163099'>
+          <Icon name='phone' size='big' /> 0770 416 3099
+        </a>
+        <Divider hidden />
+        <a href='https://www.linkedin.com/in/benfurber/'>
+          <Icon name='linkedin' size='big' />@benfurber
+        </a>
+        <Divider hidden />
+        <a href='https://github.com/benfurber/CV'>
+          <Icon name='github' size='big' />@benfurber
+        </a>
+        <Divider hidden />
       </Container>
     );
   }
