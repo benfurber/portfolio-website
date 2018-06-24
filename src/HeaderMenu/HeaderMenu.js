a66dd027bb2c5c9f5d51caaca3c4ef85c9a7ff4e
benfurber <ben@furber-consultancy.com>
2018-06-24 17:18:19 +0100

    Tidy up header icon hover text

--- src/HeaderMenu/HeaderMenu.js
+++ src/HeaderMenu/HeaderMenu.js
@@ -5,11 +5,11 @@ class HeaderMenu extends Component {
   render() {
     return (
       <div className="HeaderMenu">
-        <MenuIcon label="Home" labelHeader="Home" labelContent="" iconName="home" linkPath='/' />
-        <MenuIcon label="About" labelHeader="About Ben" labelContent="Ben's experience, skills and education" iconName="user" linkPath='/about' />
-        <MenuIcon label="Projects" labelHeader="Projects" labelContent="Recent projects undertaken as a developer" iconName="flask" linkPath='/projects' />
-        <MenuIcon label="Articles" labelHeader="Articles" labelContent="Random articles and blogs written over the years" iconName="newspaper outline" linkPath='/articles' />
-        <MenuIcon label="Contact" labelHeader="Contact" labelContent="" iconName="at" linkPath='/contact' />
+        <MenuIcon label="Home" labelHeader="Home" labelContent="The page where it all begins." iconName="home" linkPath='/' />
+        <MenuIcon label="About" labelHeader="About Ben" labelContent="Ben's experience, skills and education." iconName="user" linkPath='/about' />
+        <MenuIcon label="Projects" labelHeader="Projects" labelContent="Recent projects undertaken as a developer." iconName="flask" linkPath='/projects' />
+        <MenuIcon label="Articles" labelHeader="Articles" labelContent="Random articles and blogs written over the years." iconName="newspaper outline" linkPath='/articles' />
+        <MenuIcon label="Contact" labelHeader="Contact" labelContent="Emails and all the social media." iconName="at" linkPath='/contact' />
       </div>
     );
   }
