dfbf73a695ca7b50fb61cb3a491f0e41c52b0a30
benfurber <ben@furber-consultancy.com>
2018-06-25 07:35:22 +0100

    Remove header icon popup as it isn't working right on mobile

--- src/MenuIcon/MenuIcon.js
+++ src/MenuIcon/MenuIcon.js
@@ -6,21 +6,12 @@ import { Link } from 'react-router-dom'
 class MenuIcon extends Component {
   render() {
     const label = this.props.label
-    const labelHeader = this.props.labelHeader
-    const labelContent = this.props.labelContent
     const iconName = this.props.iconName
     const linkPath = this.props.linkPath
 
     return (
       <Link to={linkPath}>
-        <Popup
-          trigger={ <Icon circular inverted link color='teal' aria-label={label} size='large' name={iconName} /> }
-          header={labelHeader}
-          content={labelContent}
-          size='small'
-          position='bottom center'
-          on='hover'
-        />
+        <Icon circular inverted link color='teal' aria-label={label} size='large' name={iconName} /> }
       </Link>
     );
   }
