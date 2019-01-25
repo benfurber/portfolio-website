b74a140337b48e21afe8b20f28cf7d4f3ba778ca
Ben Furber <ben@fu.rber.io>
2019-01-25 17:22:12 +0000

    ✨ Add route to typeform form
    And tidy up formatting on file.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-import HomeContent from './ContentBody/HomeContent'
-import AboutContent from './ContentBody/AboutContent'
-import ProjectsContent from './ContentBody/ProjectsContent'
-import ArticlesContent from './ContentBody/ArticlesContent'
-import ContactContent from './ContentBody/ContactContent'
+import HomeContent from "./ContentBody/HomeContent";
+import AboutContent from "./ContentBody/AboutContent";
+import ProjectsContent from "./ContentBody/ProjectsContent";
+import ArticlesContent from "./ContentBody/ArticlesContent";
+import ContactContent from "./ContentBody/ContactContent";
 
-import { Container, Divider } from 'semantic-ui-react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { Container, Divider } from "semantic-ui-react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import AppHeader from './AppHeader/AppHeader'
+import AppHeader from "./AppHeader/AppHeader";
 
 class App extends Component {
   render() {
@@ -25,10 +25,19 @@ class App extends Component {
             <Route path="/articles/" component={ArticlesContent} />
             <Route path="/contact/" component={ContactContent} />
 
-            <Route path="/dog" component={() =>
-                window.location = "https://www.amazon.co.uk/Ben-Furber-Dog-Translator/dp/B07B28NH9G/ref=sr_1_1?s=digital-skills&ie=UTF8&qid=1523111280&sr=1-1&keywords=dog+translator"
-            } />
-
+            <Route
+              path="/dog"
+              component={() =>
+                (window.location =
+                  "https://www.amazon.co.uk/Ben-Furber-Dog-Translator/dp/B07B28NH9G/ref=sr_1_1?s=digital-skills&ie=UTF8&qid=1523111280&sr=1-1&keywords=dog+translator")
+              }
+            />
+            <Route
+              path="/feedback"
+              component={() =>
+                (window.location = "https://benfurber.typeform.com/to/IugCMF")
+              }
+            />
           </Switch>
         </Container>
       </Router>
