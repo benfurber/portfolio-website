c51965764e4e84b6d5e9fb29387e98dc29f0592c
Ben Furber <ben@fu.rber.io>
2019-11-02 20:26:07 +0000

    🗑 Drop projects link from header

--- src/HeaderMenu/HeaderMenu.js
+++ src/HeaderMenu/HeaderMenu.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
-import { Button } from 'semantic-ui-react'
-import MenuIcon from '../MenuIcon/MenuIcon'
+import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
+import MenuIcon from "../MenuIcon/MenuIcon";
 
 class HeaderMenu extends Component {
   render() {
     return (
       <div className="HeaderMenu">
-        <MenuIcon label="Home" iconName="home" linkPath='/' />
-        <MenuIcon label="About" iconName="user" linkPath='/about' />
-        <MenuIcon label="Projects" iconName="flask" linkPath='/projects' />
-        <MenuIcon label="Articles" iconName="newspaper outline" linkPath='/articles' />
-        <MenuIcon label="Contact" iconName="at" linkPath='/contact' />
-        <a href='https://github.com/benfurber/cv/'><Button basic inverted>CV on GitHub</Button></a>
+        <MenuIcon label="Home" iconName="home" linkPath="/" />
+        <MenuIcon label="About" iconName="user" linkPath="/about" />
+        <MenuIcon
+          label="Articles"
+          iconName="newspaper outline"
+          linkPath="/articles"
+        />
+        <MenuIcon label="Contact" iconName="at" linkPath="/contact" />
+        <a href="https://github.com/benfurber/cv/">
+          <Button basic inverted>
+            CV on GitHub
+          </Button>
+        </a>
       </div>
     );
   }
